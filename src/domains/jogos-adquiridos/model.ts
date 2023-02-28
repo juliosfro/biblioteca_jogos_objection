5b11182de6164e19cd959685d73341564b33d235
Julio de Oliveira Neto <juliosfro@gmail.com>
2023-02-28 09:46:02 -0300

    Quantidade de jogos adquiridos por usuario

--- src/domains/jogos-adquiridos/model.ts
+++ src/domains/jogos-adquiridos/model.ts
@@ -1,7 +1,7 @@
 import { AnyQueryBuilder, ModelOptions, QueryContext } from 'objection';
 import Jogo from '~/domains/jogos/model';
 import Usuario from '~/domains/usuarios/model';
-import BaseModel from '~/models';
+import BaseModel, { knex } from '~/models';
 
 class JogoAdquirido extends BaseModel {
 
@@ -64,6 +64,7 @@ class JogoAdquirido extends BaseModel {
             },
 
             getListModifier(builder: AnyQueryBuilder) {
+                builder.distinct('jogos_adquiridos.id_usuario');
                 builder.withGraphFetched({
                     usuario: {
                         jogos_adquiridos: {
@@ -73,6 +74,14 @@ class JogoAdquirido extends BaseModel {
                         },
                     },
                 });
+
+                builder.select(builderCount => {
+                    builderCount.count('jogos_adquiridos.id_jogo');
+                    builderCount.as('total_jogos_adquiridos');
+                });
+                
+                builder.whereIn('jogos_adquiridos.id_usuario', knex('usuarios').select('id'));
+                builder.groupBy('jogos_adquiridos.id_usuario');
             },
             
             getLoginModifier(_query: AnyQueryBuilder) {
@@ -80,7 +89,6 @@ class JogoAdquirido extends BaseModel {
         };
 
     }
-
 }
 
 export default JogoAdquirido;
