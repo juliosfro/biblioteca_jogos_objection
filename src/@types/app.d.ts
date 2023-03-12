1c873b32fa26c4f202ed6442f892a9722abd38c9
Julio de Oliveira Neto <juliosfro@gmail.com>
2023-03-12 09:14:31 -0300

    Criacao de rota personalizada

--- src/@types/app.d.ts
+++ src/@types/app.d.ts
@@ -1,4 +1,35 @@
 declare namespace App {
+    declare namespace Router {
+        type SwaggerContentType = 'multipart/form-data' | 'application/json';
+
+        type SwaggerParameterObject = import('openapi-types').OpenAPIV3.OpenAPIV3.ParameterObject |
+            import('openapi-types').OpenAPIV3.OpenAPIV3.ReferenceObject;
+
+        type SwaggerCustomRouteObject = {
+            contentType?: SwaggerContentType;
+            errorCodes?: number[];
+        };
+
+        type SwaggerRouteObject = import('openapi-types').OpenAPIV3.OperationObject<SwaggerCustomRouteObject>;
+
+        type RouteValidation = import('joi').AnySchema | {
+            schema: import('joi').AnySchema;
+            options?: import('joi').ValidationOptions;
+        };
+
+        type Route = {
+            method: 'all' | 'get' | 'post' | 'put' | 'delete' | 'patch' | 'options' | 'head';
+            path: string;
+            handlers: Array<import('express-serve-static-core').RequestHandlerParams>;
+            public?: boolean;
+            internal?: boolean;
+            validationBody?: RouteValidation,
+            validationQuery?: RouteValidation,
+            validationParams?: RouteValidation,
+            swagger?: SwaggerRouteObject;
+        };
+    }
+
     type FilterOrderQuery = {
         filters: import('~/helpers/request-query').Filter[],
         orders: import('~/helpers/request-query').OrderTuple[],
